ae528e5801ddef2dbbd295716b1771f3af5d8cdf
Kirill Parkhomenko <kxrxh.contact@gmail.com>
2024-05-30 02:40:13 +0300

    microcode in progress

--- machine.py
+++ machine.py
@@ -1,18 +1,10 @@
-# class MicroProgram:
-#     def __init__(self):
-#         self.microprograms = {}
-
-#     def add_microprogram(self, instruction, microprogram):
-#         self.microprograms[instruction] = microprogram
-
-#     def get_microprogram(self, instruction):
-#         return self.microprograms.get(instruction, [])
-
+from enum import Enum
 import logging
 import sys
 from typing import List
 
-from machine_error import InvalidRegisterIndex
+from machine_error import InvalidRegisterIndex, MachineRuntimeError
+from microcode import AluOperation, Mux
 
 
 class RegisterFile:
@@ -31,8 +23,8 @@ class RegisterFile:
     rip: int
     rpc: int
 
-    reg_left_out: int
-    reg_right_out: int
+    mux_left_out: Mux
+    mux_right_out: Mux
 
     def __init__(self):
         self.r0 = 0
@@ -48,12 +40,9 @@ class RegisterFile:
         self.r10 = 0
         self.r11 = 0
         self.r12 = 0
-        self.r13 = 0  # ?? Data register
-        self.r14 = 0  # ??
-        self.r15 = 0  # ??
-
-        self.reg_left_out = 0
-        self.reg_right_out = 0
+        self.r13 = 0
+        self.r14 = 0
+        self.r15 = 0
 
     def latch_reg_n(self, index: int, value: int) -> None:
         if index == 0:
@@ -63,27 +52,49 @@ class RegisterFile:
         else:
             raise InvalidRegisterIndex(f"register r{index} does not exist")
 
-    def sel_left_reg(self, index: int):
-        if index < 0 or index > 15:
-            raise InvalidRegisterIndex(f"register r{index} does not exist")
-        self.reg_left_out = getattr(self, f"r{index}")
+    def sel_left_reg(self, signal: Mux):
+        if int(Mux.SEL_R0) > int(signal) or int(Mux.SEL_R15) < int(signal):
+            raise MachineRuntimeError(
+                f"invalid select register signal: {signal}")
+        self.mux_left_out = signal
 
-    def sel_right_reg(self, index: int):
-        if index < 0 or index > 15:
-            raise InvalidRegisterIndex(f"register r{index} does not exist")
-        self.reg_right_out = getattr(self, f"r{index}")
+    def sel_right_reg(self, signal: Mux):
+        self.mux_right_out = signal
+
+    def get_left_out(self) -> int:
+        return self.get_register_by_signal(self.mux_left_out)
+
+    def get_right_out(self) -> int:
+        return self.get_register_by_signal(self.mux_right_out)
+
+    @staticmethod
+    def get_register_by_signal(signal: Mux) -> int:
+        signal_int = int(signal) - int(Mux.SEL_R0)
+        if signal_int < 0 or signal_int > 15:
+            raise MachineRuntimeError(
+                f"Invalid select register signal: {signal}")
+        return signal_int
 
 
 class Alu:
     zero_flag: int
 
+    result: int
+
     def __init__(self):
+        self.result = 0
         self.zero_flag = 0
 
-    def perform(self, in_left: int, in_right: int, operation: int) -> int:
-        result: int = 0
-
-        self.update_flags(result)
+    def perform(self, in_left: int, in_right: int, operation: AluOperation) -> int:
+        match operation:
+            case AluOperation.ADD: self.result = in_left + in_right
+            case AluOperation.SUB: self.result = in_left - in_right
+            case AluOperation.MUL: self.result = in_left * in_right
+            case AluOperation.AND: self.result = in_left & in_right
+            case _:
+                raise MachineRuntimeError(
+                    f"operation {operation} not supported by alu")
+        self.update_flags(self.result)
         return 0
 
     def update_flags(self, value: int):
@@ -91,56 +102,141 @@ class Alu:
 
 
 class ControlUnit:
+    rb: int  # 16 bit
+    r1: int  # 16 bit
+    r2: int  # 16 bit
+    tick_count: int
+
+    mpc: int
+
+    address_out: int
+    data_out: int
+
+    mpc_mux: Mux
+    operand_mux: Mux
+
     def __init__(self):
-        self.op1 = 0
-        self.op2 = 0
-        self.tmp = 0
+        self.rb = 0
+        self.r1 = 0
+        self.r2 = 0
+        self.mpc = 0
+        self.opcode_mux = self.OpcodeMux.Opcode
 
-        self.pc = 0
+    def decode_instruction(self, instruction: str) -> None:
+        opcode = int(instruction[0:7], 2)
+        flag = int(instruction[-1], 2)
+        print(opcode + flag)
 
-    def execute_micro_instruction(self, micro_instruction: str):
-        if micro_instruction == "fetch":
-            self.pc += 1
-        elif micro_instruction == "decode":
-            self.pc += 1
-        elif micro_instruction == "load_op1":
-            self.pc += 1
-        elif micro_instruction == "load_op2":
-            self.pc += 1
-        elif micro_instruction == "store_op0":
-            self.pc += 1
-        elif micro_instruction == "add":
-            self.tmp = self.op1 + self.op2
-        elif micro_instruction == "sub":
-            self.pc += 1
-        elif micro_instruction == "mul":
-            self.pc += 1
-        elif micro_instruction == "div":
-            self.pc += 1
-        elif micro_instruction == "halt":
-            self.pc += 1
-        else:
-            raise RuntimeError("Invalid micro instruction")
+    def set_address(self, address: int):
+        self.address_out = address
+
+    def set_data(self, data: int):
+        self.data_out = data
+
+    def tick(self):
+        self.tick_count += 1
+
+
+class IO:
+    input_buffer: List[int]
+    output_buffer: List[int]
+
+    def __init__(self, input: str):
+        self.input_buffer = list(map(ord, list(input)))  # str to int array
+        self.output_buffer = []
+
+    def latch_output(self, value: int):
+        self.output_buffer[-1] = value
+
+    def next_write_chr(self):
+        self.output_buffer.append(0)
+
+    def latch_input(self) -> int:
+        return self.input_buffer[0]
+
+    def next_read_chr(self):
+        self.input_buffer.pop(0)
+
+
+class DataMemory:
+    cells: List[int]
+
+    def __init__(self) -> None:
+        self.cells = []
+
+    def get_cell(self, index: int) -> int:
+        return self.cells[index]
+
+    def set_cell(self, index: int, value: int):
+        self.cells[index] = value
 
 
 class DataPath:
+    data_memory: DataMemory
     register_file: RegisterFile
     alu: Alu
-    pc: int
+    control_unit: ControlUnit
+    io_buffer: IO
 
-    def __init__(self):
+    pc: int
+    pc_mux: Mux
+
+    data_src_mux: Mux
+    data_src_mux2: Mux
+
+    def __init__(self, input: str):
         self.pc = 0
-        self.tick_count = 0
         self.alu = Alu()
         self.register_file = RegisterFile()
+        self.control_unit = ControlUnit()
+        self.io_buffer = IO(input)
+        self.data_memory = DataMemory()
+
+    def latch_pc(self) -> None:
+        match self.pc_mux:
+            case Mux.PC_CU:
+                self.pc = self.control_unit.address_out
+            case Mux.PC_INC:
+                self.pc += 1
+            case _:
+                raise MachineRuntimeError(f"invalid mux value {self.pc_mux}")
+
+    def sel_pc(self, value: Mux):
+        self.pc_mux = value
+
+    def sel_data_src(self, value: Mux):
+        self.data_src_mux = value
+
+    def sel_data_src2(self, value: Mux):
+        self.data_src_mux2 = value
+
+    def get_data_src_mux_output(self) -> int:
+        match self.data_src_mux:
+            case Mux.SRC_MEM:
+                return self.data_memory.get_cell(self.register_file.get_left_out())
+            case Mux.SRC_ALU:
+                return self.alu.result
+            case _:
+                raise MachineRuntimeError(
+                    f"invalid mux value  {self.data_src_mux}")
+
+    def get_data_src_mux2_output(self) -> int:
+        match self.data_src_mux2:
+            case Mux.SRC_CU:
+                return self.control_unit.data_out
+            case Mux.SRC_MUX:
+                return self.get_data_src_mux_output()
+            case _:
+                raise MachineRuntimeError(
+                    f"invalid mux value   {self.data_src_mux2}")
 
 
-def run_simulation(instruction_memory: List[str], data_memory: List[int]):
-    datapath = DataPath()
+# def run_simulation(instruction_memory: List[str], data_memory: List[int], input: str):
+#     datapath = DataPath(input=input)
 
-    control_unit = ControlUnit()
+#     control_unit = ControlUnit()
 
-    instruction_counter = 0
+#     instruction_counter = 0
 
 
 def read_file(file_name: str) -> List[str]:
--- microcode.py
+++ microcode.py
@@ -1,17 +1,96 @@
-from enum import Enum
+from enum import Enum, IntEnum
 
 
-class Latch(Enum):
-    PC = 0
+# Signals
+class Latch(IntEnum):
+    R0 = 0
+    R1 = 1
+    R2 = 2
+    R3 = 3
+    R4 = 4
+    R5 = 5
+    R6 = 6
+    R7 = 7
+    R8 = 8
+    R9 = 9
+    R10 = 10
+    R11 = 11
+    R12 = 12
+    R13 = 13
+    R14 = 14
+    R15 = 15
+
+    PC = 16
+
+    WRITE_BUF = 17
+    READ_BUF = 18
+
+    NEXT_READ_CH = 19
+    NEXT_WRITE_CH = 20
+
+    MPC = 21
+
+    IR = 22
 
 
-class AluOperation(Enum):
+class AluOperation(IntEnum):
     ADD = 1
     SUB = 2
-    AND = 3
-    OR = 4
-    XOR = 5
-    MUL = 6
-    MOD = 7
+    MUL = 3
+    AND = 4
 
+class Halt(IntEnum):
+    pass
+
+class Mux(IntEnum):
+    PC_CU = 0
+    PC_INC = 1
+
+    SRC_MEM = 2
+    SRC_ALU = 3
+
+    SRC_CU = 4
+    SRC_MUX = 5
+
+    SEL_R0 = 6
+    SEL_R1 = 7
+    SEL_R2 = 8
+    SEL_R3 = 9
+    SEL_R4 = 10
+    SEL_R5 = 11
+    SEL_R6 = 12
+    SEL_R7 = 13
+    SEL_R8 = 14
+    SEL_R9 = 15
+    SEL_R10 = 16
+    SEL_R11 = 17
+    SEL_R12 = 18
+    SEL_R13 = 19
+    SEL_R14 = 20
+    SEL_R15 = 21
+
+    MPC_ZERO = 22
+    MPC_INC = 23
+    MPC_IR = 24
+
+    OP_RB = 25
+    OP_R1 = 26
+    OP_R2 = 27
+
+
+# Microcommands
+mc_memory = [
+    # Instruction fetch
+    [Mux.MPC_INC, Latch.MPC, Mux.MPC_INC, Latch.PC],
+    [Mux.MPC_INC, Latch.MPC, Latch.IR],
+    [Mux.MPC_IR, Latch.PC],
     
+    # 3: Nop
+    [Mux.MPC_ZERO, Latch.MPC],
+
+    # 4: Halt
+    [Halt(0)],
+
+    # 5: Add no-flag
+    [Mux.MPC_ZERO, Latch.MPC, AluOperation.ADD],
+]
